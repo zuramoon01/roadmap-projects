07dc9f0f31700900f8a32bb41ce8f1f2ebd5d957
zuramoon01 <zuramoon01@gmail.com>
2024-10-28 21:46:38 +0700

    project: Single Page CV

---
+++ src/routes/+page.svelte
@@ -0,0 +1,173 @@
+<svelte:head>
+  <!-- SEO Meta Tags -->
+  <meta
+    name="author"
+    content="Rizki Azzura Saifani Purba"
+  />
+  <meta
+    name="description"
+    content="Single Page CV of Rizki Azzura Saifani Purba, a Software Engineer."
+  />
+  <meta
+    name="keywords"
+    content="Frontend Developer, Backend Developer, Full Stack Developer, Software Engineer, Resume, CV"
+  />
+
+  <!-- Open Graph Meta Tags -->
+  <meta
+    property="og:author"
+    content="Rizki Azzura Saifani Purba"
+  />
+  <meta
+    property="og:title"
+    content="Rizki Azzura Saifani Purba - Single Page CV"
+  />
+  <meta
+    property="og:description"
+    content="Single Page CV of Rizki Azzura Saifani Purba, a Software Engineer."
+  />
+  <meta
+    property="og:type"
+    content="website"
+  />
+  <meta
+    property="og:url"
+    content="https://www.linkedin.com/in/zuramoon/"
+  />
+  <meta
+    property="og:image"
+    content="cv.webp"
+  />
+</svelte:head>
+
+<main class="flex min-h-dvh w-full flex-col items-start gap-4 p-16">
+  <div class="w-full border-t-2 border-black pt-4">
+    <h1 class="text-3xl leading-none">Rizki Azzura Saifani Purba</h1>
+  </div>
+
+  <section class="contents">
+    <h2 class="font-semibold leading-none text-green-700">Software Engineer</h2>
+
+    <div class="font-medium leading-tight">
+      <address class="not-italic">
+        <p>Tj. Garbus II, Kec. Pagar Merbau, Kabupaten Deli Serdang,</p>
+        <p>Sumatera Utara 20551</p>
+      </address>
+      <p class="tabular-nums">+62-821-1584-6645</p>
+      <p>zuramoon01@gmail.com</p>
+    </div>
+  </section>
+
+  <section class="contents">
+    <h2 class="font-semibold leading-none text-green-700">Skills</h2>
+
+    <p class="font-medium leading-tight">
+      HTML5, CSS3, SASS, TailwindCSS, JavaScript, TypeScript, ReactJs, NextJs, Svelte, SvelteKit,
+      NodeJs, ExpressJs, PHP, Laravel, SQL, MySQL, PostgreSQL, Firebase, G
+    </p>
+  </section>
+
+  <section class="contents">
+    <h2 class="font-semibold leading-none text-green-700">Education</h2>
+
+    <article class="flex w-full flex-col items-start gap-2">
+      <div class="flex w-full flex-col items-start">
+        <h3 class="text-lg font-semibold leading-none text-blue-700">
+          Mikroskil University, Indonesia - Bachelor Degree of Informatics Engineering
+        </h3>
+
+        <time>October 2020 to October 2024</time>
+      </div>
+
+      <ul class="list-inside list-disc">
+        <li>GPA 3.98</li>
+        <li>Became Assistant Lecturer starting from semester 4</li>
+        <li>Received the Academic Achievement Scholarship in 2023</li>
+      </ul>
+    </article>
+  </section>
+
+  <section class="contents">
+    <h2 class="font-semibold leading-none text-green-700">Experience</h2>
+
+    <article class="flex w-full flex-col items-start gap-2">
+      <div class="flex w-full flex-col items-start">
+        <h3 class="text-lg font-semibold leading-none text-blue-700">
+          Universitas Mikroskil, Medan, Sumatera Utara - Full Stack Engineer
+        </h3>
+
+        <time>August 2023 to October 2023</time>
+      </div>
+
+      <ul class="list-inside list-disc">
+        <li>
+          Carrying out testing, debugging, troubleshooting and fixing bugs when receiving reports
+          from IT Support regarding problems experienced by students or employees on the Mikroskil
+          website.
+        </li>
+        <li>Develop new features in accordance with the terms and conditions provided.</li>
+        <li>Collaborate on problem solving and feature development with the team.</li>
+        <li>Active in providing ideas regarding work.</li>
+      </ul>
+
+      <p>Technologies: HTML5, CSS3, Bootstrap, Javascript, JQuery, PHP, Symfony, Laravel, Lumen</p>
+    </article>
+
+    <article class="flex w-full flex-col items-start gap-2">
+      <div class="flex w-full flex-col items-start">
+        <h3 class="text-lg font-semibold leading-none text-blue-700">
+          Universitas Mikroskil, Medan, Sumatera Utara - Assistant Lecturer (part-time)
+        </h3>
+
+        <time>May 2022 to July 2022</time>
+      </div>
+
+      <ul class="list-inside list-disc">
+        <li>
+          Assisted lecturers in teaching students. Helped students solving their problems. Prepared
+          the lab before using it.
+        </li>
+      </ul>
+
+      <p>
+        Subjects: Computer Programming, Computational Thinking, Front End Web Development, Back End
+        Web Development, Front End Mobile Applications Development, Back End Mobile Applications
+        Development
+      </p>
+    </article>
+
+    <article class="flex w-full flex-col items-start gap-2">
+      <div class="flex w-full flex-col items-start">
+        <h3 class="text-lg font-semibold leading-none text-blue-700">
+          PT. Pundi Mas Berjaya, Medan, Sumatera Utara - Back End Web Developer Intern
+        </h3>
+
+        <time>August 2022 to December 2023</time>
+      </div>
+
+      <ul class="list-inside list-disc">
+        <li>
+          Collaborate with the front-end team and system analysts in building a Web Builder (TRY US)
+          application with the Laravel back-end framework.
+        </li>
+        <li>
+          Manage and optimize the database according to the design of the system analyst team.
+        </li>
+        <li>Design and implement scalable and efficient RESTful APIs.</li>
+        <li>Carrying out application testing and debugging to ensure performance and security.</li>
+        <li>Conduct code reviews to ensure code quality and consistency.</li>
+      </ul>
+
+      <p>Technologies : Laravel</p>
+    </article>
+  </section>
+
+  <section class="flex flex-col items-start gap-2">
+    <h2 class="font-semibold leading-none text-green-700">Across the Internet</h2>
+
+    <div>
+      <a href="https://www.linkedin.com/in/zuramoon/">Linked - zuramoon</a>,
+      <a href="https://github.com/zuramoon01">Github - zuramoon01</a>
+    </div>
+  </section>
+</main>
